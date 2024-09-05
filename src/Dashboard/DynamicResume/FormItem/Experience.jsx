d11201aa105ee57986dc0ee31495c9e6ad45a77e
AzizulHakim18 <hakimrony18@gamil.com>
2024-09-06 03:24:06 +0600

    error make

--- src/Dashboard/DynamicResume/FormItem/Experience.jsx
+++ src/Dashboard/DynamicResume/FormItem/Experience.jsx
@@ -73,7 +73,7 @@ const Experience = () => {
                 <div>
                     {
                         experinceList.map((item, index) => (
-                            <div>
+                            <div key={index}>
                                 <div className='grid grid-cols-2 gap-3 border p-3 my-5 rounded-lg'>
                                     <div >
                                         <label className='text-xs'>Postion Title</label>
--- src/Dashboard/DynamicResume/RichTextEditor.jsx
+++ src/Dashboard/DynamicResume/RichTextEditor.jsx
@@ -1,10 +1,53 @@
-import React, { useState } from 'react';
+import { ResumeInfoContext } from '@/ContextInfo/ResumeInfoContext';
+import React, { useContext, useState } from 'react';
+import toast from 'react-hot-toast';
 import { BtnBold, BtnBulletList, BtnItalic, BtnLink, BtnNumberedList, BtnStrikeThrough, BtnUnderline, Editor, EditorProvider, Separator, Toolbar } from 'react-simple-wysiwyg';
+import { AIChatSession } from '../../../service/AiModel';
+import { Button } from '@/components/ui/button';
+import { Brain, LoaderCircle } from 'lucide-react';
 
-const RichTextEditor = ({ onRichTextEditorChange }) => {
+
+
+const PROMPT = 'position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and No JSON array) , give me result in HTML tags'
+
+
+const RichTextEditor = ({ onRichTextEditorChange, index, defaultValue }) => {
     const [value, setValue] = useState();
+    const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext)
+    const [loading, setLoading] = useState(false);
+
+    const GenerateSummeryFromAI = async () => {
+        setLoading(true)
+        if (!resumeInfo?.Experience[index]?.title) {
+            toast.error('Please Add Position Title');
+            return;
+        }
+        setLoading(true)
+        const prompt = PROMPT.replace('{positionTitle}', resumeInfo.Experience[index].title);
+
+        const result = await AIChatSession.sendMessage(prompt);
+        console.log(result.response.text());
+        const resp = result.response.text()
+        setValue(resp.replace('[', '').replace(']', ''));
+        setLoading(false);
+    }
+
     return (
         <div>
+            <div className='flex justify-between my-2'>
+                <label className='text-xs'>Summery</label>
+                <Button variant="outline" size="sm"
+                    onClick={GenerateSummeryFromAI}
+                    disabled={loading}
+                    className="flex gap-2 border-primary text-primary">
+                    {loading ?
+                        <LoaderCircle className='animate-spin' /> :
+                        <>
+                            <Brain className='h-4 w-4' /> Generate from AI
+                        </>
+                    }
+                </Button>
+            </div>
             <EditorProvider >
                 <Editor value={value} onChange={(e) => {
                     setValue(e.target.value);
